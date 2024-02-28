0d5dd1e91c418e692e3baf0bf361f32e9d70d3d9
Koffi GREBE <yvan.grebe@email.com>
2024-02-28 09:30:18 +0100

    property_details_printhouse

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+// import data
+import { housesData } from '../data';
+
+// import use params
+import { useParams } from 'react-router-dom';
+
+// import icons
+import { BiBed, BiBath, BiArea } from 'react-icons/bi';
+
+// import 
+import { Link } from 'react-router-dom';
+
 const PropertyDetails = () => {
+  // get the house id
+  const { id } = useParams();
+  
+  // get the house based on the id
+const house = housesData.find((house) => {
+  return house.id === parseInt(id);
+});
+
+console.log(house);
+
   return <div>PropertyDetails</div>;
 };
+
 
 export default PropertyDetails;
