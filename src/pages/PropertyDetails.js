669c0bacfc5c05f1c521132fb4b0e237b7924ae8
Koffi GREBE <yvan.grebe@email.com>
2024-02-29 08:42:55 +0100

    footer

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -74,6 +74,36 @@ const house = housesData.find((house) => {
               </Link>
             </div>
           </div>
+          {/* form */}
+          <form className='flex flex-col gap-y-4'>
+            <input 
+            className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm'
+            type='text' 
+            placeholder='Name*'
+            />
+            <input 
+            className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm'
+            type='text' 
+            placeholder='Email*'
+            />
+            <input 
+            className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm'
+            type='text' 
+            placeholder='Phone*'
+            />
+            <textarea className='border border-gray-300 focus-violet-700 outline-none resize-none rounded w-full p-4 h-36 text-sm text-gray-400'
+            placeholder='Message*'
+            defaultValue='Hello, I am interested in [Modern apartment]'
+            ></textarea>
+            <div className='flex flex-col gap-y-4'>
+              <button className='bg-violet-700 hover:bg-violet-800 text-white rounded p-4 text-sm w-full transition'>
+                Send message
+              </button>
+              <button className='border border-violet-700 text-violet-700 hover:border-violet-500 hover:text-violet-500 rounded p-4 text-sm w-full transition'>
+                Call
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
