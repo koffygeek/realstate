8ec5f5e45212d034be525935777a6914d0bf36c4
Koffi GREBE <yvan.grebe@email.com>
2024-02-29 09:00:25 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-black py-8 text-center text-white'>
+    <div className='container mx-auto'>
+      copyright & copy; 2024, All rights reserved.
+    </div>
+    </footer>
+  );
 };
 
 export default Footer;
